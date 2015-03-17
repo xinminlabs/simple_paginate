98ee6057a1d480866d00b8c452c6a1b0005314d1
Yong Gu <zerogy921@gmail.com>
2015-03-17 23:39:14 +0800

    Include extension when class is initialized.

--- lib/simple_paginate/models/active_record_extension.rb
+++ lib/simple_paginate/models/active_record_extension.rb
@@ -1,52 +1,19 @@
+require 'simple_paginate/models/active_record_model_extension'
+
 module SimplePaginate
   module ActiveRecordExtension
     extend ActiveSupport::Concern
 
-    included do
-      def self.paginate(options = {})
-        options = options.dup
-        page = options.delete(:page) || 1
-        per_page = options.delete(:per_page) || SimplePaginate.config.default_per_page
-
-        offset(per_page * (page - 1)).limit(per_page + 1).extending do
-          def current_page
-            (offset_value / (limit_value - 1)) + 1
-          end
-
-          def first_page?
-            current_page == 1
-          end
-
-          def last_page?
-            loaded? ? @actual_records.length < limit_value : count(:all) < limit_value
-          end
-
-          def per(num)
-            if (n = num.to_i) < 0 || !(/^\d/ =~ num.to_s)
-              self
-            elsif n.zero?
-              limit(n + 1)
-            else
-              limit(n + 1).offset(offset_value / (limit_value - 1) + 1)
-            end
-          end
-
-          private
-
-          def exec_queries
-            @actual_records = super
-            @records = if @actual_records.length > 1 && @actual_records.length == limit_value
-                         @actual_records.first(limit_value - 1)
-                       else
-                         @actual_records
-                       end
-            @records
-          end
-        end
+    module ClassMethods
+      def inherited(kls)
+        super
+        kls.send(:include, SimplePaginate::ActiveRecordModelExtension)
       end
+    end
 
-      def self.page(num = nil)
-        self.paginate(page: num)
+    included do
+      self.descendants.each do |kls|
+        kls.send(:include, SimplePaginate::ActiveRecordModelExtension)
       end
     end
   end
---
+++ lib/simple_paginate/models/active_record_model_extension.rb
@@ -0,0 +1,53 @@
+module SimplePaginate
+  module ActiveRecordModelExtension
+    extend ActiveSupport::Concern
+
+    included do
+      def self.paginate(options = {})
+        options = options.dup
+        page = options.delete(:page) || 1
+        per_page = options.delete(:per_page) || SimplePaginate.config.default_per_page
+
+        offset(per_page * (page - 1)).limit(per_page + 1).extending do
+          def current_page
+            (offset_value / (limit_value - 1)) + 1
+          end
+
+          def first_page?
+            current_page == 1
+          end
+
+          def last_page?
+            loaded? ? @actual_records.length < limit_value : count(:all) < limit_value
+          end
+
+          def per(num)
+            if (n = num.to_i) < 0 || !(/^\d/ =~ num.to_s)
+              self
+            elsif n.zero?
+              limit(n + 1)
+            else
+              limit(n + 1).offset(offset_value / (limit_value - 1) + 1)
+            end
+          end
+
+          private
+
+          def exec_queries
+            @actual_records = super
+            @records = if @actual_records.length > 1 && @actual_records.length == limit_value
+                         @actual_records.first(limit_value - 1)
+                       else
+                         @actual_records
+                       end
+            @records
+          end
+        end
+      end
+
+      def self.page(num = nil)
+        self.paginate(page: num)
+      end
+    end
+  end
+end
