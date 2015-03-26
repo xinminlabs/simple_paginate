31344fd6ac745c0c53b73462bec4a2d56c9fd34e
Yong Gu <zerogy921@gmail.com>
2015-03-27 03:58:03 +0800

    Auto corrected by following Style/RedundantSelf

--- lib/simple_paginate/models/active_record_extension.rb
+++ lib/simple_paginate/models/active_record_extension.rb
@@ -20,7 +20,7 @@ module SimplePaginate
         alias_method_chain :inherited, :simple_paginate
       end
 
-      self.descendants.each do |kls|
+      descendants.each do |kls|
         kls.send(:include, SimplePaginate::ActiveRecordModelExtension)
       end
     end
