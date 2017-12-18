196e89b33edebf424ce52d508016f44681d4a32a
Richard Huang <flyerhzm@gmail.com>
2017-12-18 10:59:42 +0800

    add focus filter to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,5 +15,6 @@ require 'rspec/rails'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
-  config.infer_spec_type_from_file_location!
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
